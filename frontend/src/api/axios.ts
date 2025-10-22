944c88780ddc7b03a955c73aa85778c5e872d7c8
rhaissav <rhaissa.vasconcelos@grupoboticario.com.br>
2025-10-22 18:34:11 -0300

    fix: update axios baseURL to use production URL

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { parseJwt } from '../utils/jwt';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL_PROD || 'http://localhost:3000',
+    baseURL: 'https://osv-do5t.onrender.com',
 });
 
 let refreshing = false;
