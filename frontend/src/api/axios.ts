4114a7f91a1690e37c576c76f51426da67917204
rhaissav <rhaissa.vasconcelos@grupoboticario.com.br>
2025-10-22 18:07:19 -0300

    fix: update axios baseURL to use environment variable for production

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { parseJwt } from '../utils/jwt';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: import.meta.env.VITE_API_URL_PROD || 'http://localhost:3000',
 });
 
 let refreshing = false;
